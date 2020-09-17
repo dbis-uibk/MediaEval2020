46a26c3210e91a16794be17785bdb6ce0b8faf8e
Michael Vötter <mikevo-uibk@famv.net>
2020-09-17 15:12:28 +0200

    Update cnn plan

--- plans/cnn.py
+++ plans/cnn.py
@@ -2,7 +2,6 @@ from dbispipeline.evaluators import FixedSplitEvaluator
 from dbispipeline.evaluators import ModelCallbackWrapper
 import dbispipeline.result_handlers
 from sklearn.ensemble import ExtraTreesClassifier
-from sklearn.multiclass import OneVsRestClassifier
 from sklearn.pipeline import Pipeline
 
 from mediaeval2020 import common
@@ -12,7 +11,7 @@ from mediaeval2020.models.cnn import CNNModel
 dataloader = MelSpectPickleLoader('data/mediaeval2020/melspect_1366.pickle')
 
 pipeline = Pipeline([
-    ("model", OneVsRestClassifier(CNNModel(epochs=8, dataloader=dataloader))),
+    ("model", CNNModel(epochs=8, dataloader=dataloader)),
 ])
 
 evaluator = ModelCallbackWrapper(
