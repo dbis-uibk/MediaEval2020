20d0ee51d54aafaa296298b97a66a365e829e810
Michael Vötter <mikevo-uibk@famv.net>
2020-10-21 16:13:12 +0200

    Add cnn plan with 1000 epochs

---
+++ plans/cnn_1000.py
@@ -0,0 +1,37 @@
+"""CNN plan with 1000 epochs."""
+from dbispipeline.evaluators import EpochEvaluator
+from dbispipeline.evaluators import ModelCallbackWrapper
+import dbispipeline.result_handlers
+from sklearn.pipeline import Pipeline
+
+from mediaeval2020 import common
+from mediaeval2020.dataloaders.melspectrograms import MelSpectPickleLoader
+from mediaeval2020.models.cnn import CNNModel
+
+dataloader = MelSpectPickleLoader('data/mediaeval2020/melspect_1366.pickle')
+
+pipeline = Pipeline([
+    (
+        'model',
+        CNNModel(
+            epochs=1000,
+            dataloader=dataloader,
+            block_sizes=[
+                32,
+                32,
+                64,
+                64,
+                64,
+            ],
+        ),
+    ),
+])
+
+evaluator = ModelCallbackWrapper(
+    EpochEvaluator(**common.fixed_split_params(), scoring_step_size=2),
+    lambda model: common.store_prediction(model, dataloader),
+)
+
+result_handlers = [
+    dbispipeline.result_handlers.print_gridsearch_results,
+]
