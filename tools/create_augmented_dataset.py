892db509f598cbb4956be380599812d8146ebd59
Michael Vötter <mikevo-uibk@famv.net>
2020-10-22 13:15:41 +0200

    Add tool to create augmented dataset pickle.

---
+++ tools/create_augmented_dataset.py
@@ -0,0 +1,36 @@
+"""Creates a MediaEval2020 dataset based on MediaEval2019."""
+import pickle
+
+from logzero import logger
+
+from mediaeval2020.dataloaders.melspectrograms import MelSpectrogramsLoader
+
+logger.info('Load data using the MelSpectrogramLoader')
+WINDOW_SIZE = 1366
+dataloader = MelSpectrogramsLoader(
+    data_path='data/mediaeval2019/melspec_data',
+    training_path='data/mediaeval2019/autotagging_moodtheme-train.tsv',
+    test_path='data/mediaeval2019/autotagging_moodtheme-test.tsv',
+    validate_path='data/mediaeval2019/autotagging_moodtheme-validation.tsv',
+    window_size=WINDOW_SIZE,
+    window='random',
+    num_windows=14,
+)
+
+file_name = 'data/mediaeval2020'
+file_name += ('/melspect_augmented_' + str(WINDOW_SIZE) + '.pickle')
+
+logger.info('Extract data')
+data = {
+    'train': dataloader.load_train(),
+    'test': dataloader.load_test(),
+    'validate': dataloader.load_validate(),
+    'configuration': dataloader.configuration,
+}
+
+logger.info('Store melspect data')
+pickle.dump(
+    data,
+    open(file_name, 'wb'),
+    protocol=pickle.HIGHEST_PROTOCOL,
+)
