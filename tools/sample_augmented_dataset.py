8821326fbd670c9cfb2c5d9edda7e8d74532e9f5
Michael Vötter <mikevo-uibk@famv.net>
2020-10-27 08:47:15 +0100

    Add tool to sample augmented data.

---
+++ tools/sample_augmented_dataset.py
@@ -0,0 +1,17 @@
+"""Selects windows from augmented dataset."""
+import pickle
+
+from logzero import logger
+
+
+def _log_shape(data, subset):
+    logger.info(subset + ':', data[subset].shape)
+
+
+file_name = 'data/mediaeval2020/melspect_augmented_1366.pickle'
+with open(file_name, 'rb') as f:
+    data = pickle.load(f)
+
+_log_shape(data, 'train')
+_log_shape(data, 'validate')
+_log_shape(data, 'test')
