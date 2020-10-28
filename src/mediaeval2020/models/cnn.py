b38985adac147ceaf99f0a6c28745e431cc2a4a5
Michael Vötter <mikevo-uibk@famv.net>
2020-10-28 14:24:56 +0100

    Fix validation code in models.

--- src/mediaeval2020/models/cnn.py
+++ src/mediaeval2020/models/cnn.py
@@ -73,7 +73,8 @@ class CNNModel(BaseEstimator, ClassifierMixin):
                 self.threshold = np.full(output_shape, .5)
 
     def validate(self, X, y):
-        y_pred = self.predict_proba(X)
+        X = self._reshape_data(X)
+        y_pred = self.model.predict(X)
         threshold = []
         for label_idx in range(y_pred.shape[1]):
             fpr, tpr, thresholds = roc_curve(y[..., label_idx],
