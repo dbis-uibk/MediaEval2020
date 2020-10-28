077b37fe525206af8ddd7da525b2c02ae86d12f1
Michael Vötter <mikevo-uibk@famv.net>
2020-10-28 14:32:34 +0100

    Add cnn linear ensamble plans for augmented sampled dataset.

---
+++ plans/augmented_sampled_ensemble_cnn_50_linear_2.py
@@ -0,0 +1,46 @@
+"""Ensemble plan linear split."""
+from dbispipeline.evaluators import EpochEvaluator
+from dbispipeline.evaluators import ModelCallbackWrapper
+import dbispipeline.result_handlers
+import numpy as np
+from sklearn.pipeline import Pipeline
+
+from mediaeval2020 import common
+from mediaeval2020.dataloaders.melspectrograms import MelSpectPickleLoader
+from mediaeval2020.models.cnn import CNNModel
+from mediaeval2020.models.ensemble import Ensemble
+
+dataloader = MelSpectPickleLoader(
+    'data/mediaeval2020/melspect_augmented_1366_sampled.pickle')
+
+label_splits = [
+    np.arange(0, 28, 1),
+    np.arange(28, 56, 1),
+]
+
+pipeline = Pipeline([
+    ('model',
+     Ensemble(
+         base_estimator=CNNModel(
+             dataloader=dataloader,
+             block_sizes=[
+                 32,
+                 32,
+                 64,
+                 64,
+                 64,
+             ],
+         ),
+         label_splits=label_splits,
+         epochs=50,
+     )),
+])
+
+evaluator = ModelCallbackWrapper(
+    EpochEvaluator(**common.fixed_split_params(), scoring_step_size=2),
+    lambda model: common.store_prediction(model, dataloader),
+)
+
+result_handlers = [
+    dbispipeline.result_handlers.print_gridsearch_results,
+]
